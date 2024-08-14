744fea676039b7da69bb847337abd17b6ffe6cc3
Storik4pro <86303380+Storik4pro@users.noreply.github.com>
2024-08-14 16:35:13 +0300

    Add files via upload

---
+++ js/faq.js
@@ -0,0 +1,175 @@
+const faqs = [
+    {
+        id: 1,
+        title: "Permission error",
+        description: "Ошибка [Errno 13] Permission error при распаковке архива",
+        page: "faq/installer/installer_windrivert-sys.html"
+    },
+    {
+        id: 2,
+        title: "[Errno 13] Permission denied",
+        description: "Решение ошибки [Errno 13], возникающей на этапе скачивания архива.",
+        page: "faq/installer/installer_permission_denied.html"
+    },
+    {
+        id: 3,
+        title: "Уведомление при запуске Update assistant",
+        description: "Что значит появляющееся уведомление \"No version specified for update\"",
+        page: "faq/installer/installer_version_not_specified.html"
+    },
+    {
+        id: 4,
+        title: "[Errno 2] No such file or directory",
+        description: "Решение ошибки [Errno 2], возникающей на этапе скачивания архива.",
+        page: "faq/installer/installer_errno2.html"
+    },
+    {
+        id: 5,
+        title: "Update assistant error",
+        description: "Решение ошибки \"Не удаётся получить корректный ответ от сервера\"",
+        page: "faq/installer/installer_assets.html"
+    },
+    {
+        id: 5,
+        title: "'NoneType' object has no attribute 'split'",
+        description: "Решение ошибки, происходящей во время подготовки к скачиванию",
+        page: "faq/installer/installer_nontype.html"
+    },
+
+];
+
+let currentPage = 1;
+const itemsPerPage = 3;
+let filteredFAQs = [];
+
+document.addEventListener("DOMContentLoaded", function() {
+    const itemsPerPage = 4;
+    let currentPage = 1;
+
+    const faqContainer = document.getElementById("faq-container");
+    const totalItems = faqs.length;
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+    const pageTitle = document.getElementById("pageTitle");
+    const searchResultsInfo = document.getElementById("search-results-info");
+    const searchResultsCount = document.getElementById("search-results-count");
+    const pagination = document.getElementById("pagination");
+    const prevPageBtn = document.getElementById("prev-page");
+    const nextPageBtn = document.getElementById("next-page");
+    const pageInfo = document.getElementById("page-info");
+
+    const notification = document.getElementById("notification");
+
+    function renderFaqs(filteredFaqs = faqs) {
+        faqContainer.innerHTML = ""; 
+
+        const start = (currentPage - 1) * itemsPerPage;
+        const end = start + itemsPerPage;
+
+        filteredFaqs.slice(start, end).forEach(faq => {
+            const faqItem = document.createElement("a");
+            faqItem.className = "faq-card";
+            faqItem.href = faq.page;
+            faqItem.innerHTML = `
+                <h2>${faq.title}</h2>
+                <p>${faq.description}</p>
+                <a href="${faq.page}">Подробнее</a>
+            `;
+            faqContainer.appendChild(faqItem);
+        });
+
+        pageInfo.textContent = `${currentPage} / ${Math.ceil(filteredFaqs.length / itemsPerPage)}`;
+        pagination.style.display = filteredFaqs.length > itemsPerPage ? "flex" : "none";
+        prevPageBtn.disabled = currentPage === 1;
+        nextPageBtn.disabled = currentPage >= Math.ceil(filteredFaqs.length / itemsPerPage);
+    }
+
+    prevPageBtn.addEventListener("click", function() {
+        if (currentPage > 1) {
+            currentPage--;
+            renderFaqs();
+        }
+    });
+
+    nextPageBtn.addEventListener("click", function() {
+        if (currentPage < totalPages) {
+            currentPage++;
+            renderFaqs();
+        }
+    });
+
+    function showSearchResults(query) {
+        const results = faqs.filter(faq => faq.title.toLowerCase().includes(query.toLowerCase()) || faq.description.toLowerCase().includes(query.toLowerCase()));
+        const resultsCount = results.length;
+
+        if (resultsCount > 0) {
+            notification.style.display = "none";
+            searchResultsInfo.style.display = "block";
+            searchResultsCount.textContent = `Результатов найдено (${resultsCount}/${totalItems})`;
+            notification.querySelector("p").textContent = `Результаты поиска: "${query}". Результатов найдено (${resultsCount}/${totalItems})`;
+
+            currentPage = 1;
+            renderFaqs(results);
+        } else {
+            searchResultsInfo.style.display = "none";
+            notification.style.display = "block";
+            notification.querySelector("p").textContent = `По запросу "${query}" ничего не найдено.`;
+        }
+    }
+
+    document.querySelector('.search-bar button').addEventListener('click', function() {
+        const query = document.querySelector('.search-bar input').value.trim();
+        if (query) {
+            showSearchResults(query);
+        } else {
+            notification.style.display = "block";
+            notification.querySelector("p").textContent = "Введите запрос для поиска.";
+        }
+    });
+
+    renderFaqs();
+});
+
+
+
+
+
+function navigateToFAQ(page) {
+    window.location.href = page;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const codeBlocks = document.querySelectorAll('.code-block');
+
+    codeBlocks.forEach(block => {
+        const language = block.getAttribute('data-language') || 'Копировать';
+        const code = block.textContent.trim();
+
+        const codeHeader = document.createElement('div');
+        codeHeader.classList.add('code-header');
+
+        const languageLabel = document.createElement('span');
+        languageLabel.classList.add('language');
+        languageLabel.textContent = language;
+
+        const copyButton = document.createElement('button');
+        copyButton.classList.add('copy-button');
+        copyButton.textContent = 'Копировать';
+        copyButton.onclick = () => {
+            navigator.clipboard.writeText(code);
+            alert('Код скопирован в буфер обмена');
+        };
+
+        codeHeader.appendChild(languageLabel);
+        codeHeader.appendChild(copyButton);
+
+        const pre = document.createElement('pre');
+        const codeElement = document.createElement('code');
+        codeElement.textContent = code;
+        pre.appendChild(codeElement);
+
+        block.innerHTML = '';
+        block.appendChild(codeHeader);
+        block.appendChild(pre);
+    });
+});
---
+++ js/faq_ui.js
@@ -0,0 +1,113 @@
+const faqs = [
+    {
+        id: 1,
+        title: "[WinError 1021] Невозможно создать статический параметр",
+        description: "Решение ошибки \"Невозможно создать статический подпараметр для временного родительского параметра\"",
+        page: "faq/goodbyeDPI_UI/ui_reg_error.html"
+    },
+    {
+        id: 2,
+        title: "Иконки в трее",
+        description: "При появлении уведомления в трее показывается несколько иконок приложения",
+        page: "faq/goodbyeDPI_UI/ui_trey_icons.html"
+    },
+    {
+        id: 3,
+        title: "Ошибка: множество иконок в трее",
+        description: "После запуска программы через галочку \"запустить после завершения\" в установщике появляется множество иконок",
+        page: "faq/goodbyeDPI_UI/ui_trey_error.html"
+    },
+
+];
+
+let currentPage = 1;
+const itemsPerPage = 3;
+let filteredFAQs = [];
+
+document.addEventListener("DOMContentLoaded", function() {
+    const itemsPerPage = 4;
+    let currentPage = 1;
+
+    const faqContainer = document.getElementById("faq-container");
+    const totalItems = faqs.length;
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+    const pageTitle = document.getElementById("pageTitle");
+    const searchResultsInfo = document.getElementById("search-results-info");
+    const searchResultsCount = document.getElementById("search-results-count");
+    const pagination = document.getElementById("pagination");
+    const prevPageBtn = document.getElementById("prev-page");
+    const nextPageBtn = document.getElementById("next-page");
+    const pageInfo = document.getElementById("page-info");
+
+    const notification = document.getElementById("notification");
+
+    function renderFaqs(filteredFaqs = faqs) {
+        faqContainer.innerHTML = ""; 
+
+        const start = (currentPage - 1) * itemsPerPage;
+        const end = start + itemsPerPage;
+
+        filteredFaqs.slice(start, end).forEach(faq => {
+            const faqItem = document.createElement("a");
+            faqItem.className = "faq-card";
+            faqItem.href = faq.page;
+            faqItem.innerHTML = `
+                <h2>${faq.title}</h2>
+                <p>${faq.description}</p>
+                <a href="${faq.page}">Подробнее</a>
+            `;
+            faqContainer.appendChild(faqItem);
+        });
+
+        pageInfo.textContent = `${currentPage} / ${Math.ceil(filteredFaqs.length / itemsPerPage)}`;
+        pagination.style.display = filteredFaqs.length > itemsPerPage ? "flex" : "none";
+        prevPageBtn.disabled = currentPage === 1;
+        nextPageBtn.disabled = currentPage >= Math.ceil(filteredFaqs.length / itemsPerPage);
+    }
+
+    prevPageBtn.addEventListener("click", function() {
+        if (currentPage > 1) {
+            currentPage--;
+            renderFaqs();
+        }
+    });
+
+    nextPageBtn.addEventListener("click", function() {
+        if (currentPage < totalPages) {
+            currentPage++;
+            renderFaqs();
+        }
+    });
+
+    function showSearchResults(query) {
+        const results = faqs.filter(faq => faq.title.toLowerCase().includes(query.toLowerCase()) || faq.description.toLowerCase().includes(query.toLowerCase()));
+        const resultsCount = results.length;
+
+        if (resultsCount > 0) {
+            notification.style.display = "none";
+            searchResultsInfo.style.display = "block";
+            searchResultsCount.textContent = `Результатов найдено (${resultsCount}/${totalItems})`;
+            notification.querySelector("p").textContent = `Результаты поиска: "${query}". Результатов найдено (${resultsCount}/${totalItems})`;
+
+            currentPage = 1;
+            renderFaqs(results);
+        } else {
+            searchResultsInfo.style.display = "none";
+            notification.style.display = "block";
+            notification.querySelector("p").textContent = `По запросу "${query}" ничего не найдено.`;
+        }
+    }
+
+    document.querySelector('.search-bar button').addEventListener('click', function() {
+        const query = document.querySelector('.search-bar input').value.trim();
+        if (query) {
+            showSearchResults(query);
+        } else {
+            notification.style.display = "block";
+            notification.querySelector("p").textContent = "Введите запрос для поиска.";
+        }
+    });
+
+    renderFaqs();
+});
