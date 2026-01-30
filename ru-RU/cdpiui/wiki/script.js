139ceb8e6c236829778dc3a29a17b31bf3af910c
Storik4pro <serstor15@gmail.com>
2026-01-30 19:31:11 +0300

    Add new wiki for cdpiui

---
+++ ru-RU/cdpiui/wiki/script.js
@@ -0,0 +1,52 @@
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+const flexContainer = document.querySelector('.flex-container');
+flexContainer.innerHTML = ""; 
+
+const wikiPages = readTextFile("/ru-RU/cdpiui/wiki/data.json", function(text){
+    data = JSON.parse(text);
+
+    data.data.forEach(group => {
+        const pages = group.pages
+        
+        const header = document.createElement('h3');
+        header.textContent = group.name;
+        header.classList.add("selection-header");
+
+        if (group.name != ".") flexContainer.appendChild(header);
+
+        pages.forEach(page => {
+            page.href = page.href.replace("\\", "\\\\")
+            const item = document.createElement('div');
+            item.className = 'flex-container-item';
+            item.innerHTML = `
+                <div class="help-container">
+                    <form class="help-container-item" action="${page.href}">
+                        <button class="legacy-button news-tile" type="button" onclick="location.href='${page.href}'">
+                            <h2 class="legacy">${page.title}</h2>
+                            <div class="link-legacy-container">
+                                <a class="link-legacy" href="${page.href}">
+                                    Перейти<span class="font-icon">&#xE76C;</span>
+                                </a>
+                            </div>
+                        </button>
+                    </form>
+                </div>
+            `;
+            flexContainer.appendChild(item);
+        });
+    });
+});
+
+
+
